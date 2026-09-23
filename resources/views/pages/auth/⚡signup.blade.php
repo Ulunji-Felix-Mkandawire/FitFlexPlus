0a2d101f38c910d031920216cde5885ff5c0cc74
Krassic Felix <felixulunjimkandawire@gmail.com>
2026-09-23 15:08:23 +0200

    Add registration logic

--- resources/views/pages/auth/⚡signup.blade.php
+++ resources/views/pages/auth/⚡signup.blade.php
@@ -2,8 +2,7 @@
 
 use Livewire\Component;
 use App\Models\User;
-
-
+use Illuminate\Support\Facades\Auth;
 
 new class extends Component {
     public string $userName;
@@ -12,9 +11,22 @@ new class extends Component {
 
     public function signup()
     {
-        $this->validate([
-            'userName' => 'required|string|mim:3|max:50'
+        // Form input validation
+        $validated = $this->validate([
+            'userName' => ['required', 'string', 'mim:3', 'max:50'],
+            'userEmail' => ['required', 'email', 'unique:users'],
+            'userPassword' => ['required', 'min:0', 'confirmed'],
         ]);
+
+        // New user creation
+        $newUser = User::create($validated);
+
+        // New user login with Auth facade
+        Auth::login($newUser);
+
+        // User Redirection
+        $this->redirect('dashboard.index', navigate: true);
+        
     }
 };
 ?>
